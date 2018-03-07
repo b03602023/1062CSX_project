816a96262789a325dbb7b68f514941535df07acb
b03602023 <b03602023@ntu.edu.tw>
2018-03-07 13:32:51 +0800

    上傳hw1

--- week_1/course_1/hw_1_question.R
+++ week_1/course_1/hw_1_question.R
@@ -37,7 +37,7 @@ m
 ########################################################### Task 3
 
 # 使用sample(), 產出10個介於10~100的整數，並存在變數 nums
-nums=sample(seq(10,100,1),10)
+nums=sample(seq(10,100,1),10,replace=TRUE)
 
 # 查看nums
 nums
@@ -47,7 +47,7 @@ nums
 for(i in 1:length(nums)){
   if((nums[i]%%2==0)&(nums[i]>50)){
     if(nums[i]==66){print("太66666666666了");break}
-    print(sprintf("偶數且大於50: %d",nums[i]))
+    cat("偶數且大於50: ",nums[i])
   }
 }
 
@@ -61,13 +61,14 @@ for(i in 1:length(nums)){
 ########################################################### Task 4
 
 # 請寫一段程式碼，能判斷輸入之西元年分 year 是否為閏年
-yr=scan()
-1995
+print("輸入西元年分判斷是否為閏年")
+yr=scan(n=1,quiet=TRUE)
+
 
 if(yr%%4==0){
-  print("閏年")
+  cat("閏年")
 }else{
-  print("非潤年")
+  cat("非潤年")
 }
 
 
@@ -82,6 +83,36 @@ if(yr%%4==0){
 # 1. 請寫一個由電腦隨機產生不同數字的四位數(1A2B遊戲)
 # 2. 玩家可重覆猜電腦所產生的數字，並提示猜測的結果(EX:1A2B)
 # 3. 一旦猜對，系統可自動計算玩家猜測的次數
+x=sample(0:9,4,replace=FALSE)
+
+
+repeat
+{
+  cat("請猜不同數字的四位數","\n","請從千位數開始輸入","\n","結束遊戲請第一個數字輸入11","\n")
+  y=scan(n=4,quiet=TRUE)
+  if(y[1]==11){
+    break
+  }
+  a=b=0
+  for(i in 1:4){
+    if(x[i]==y[i]){
+      a=a+1
+    }
+    for(j in 1:4){
+      if(x[i]==y[j]){
+        if(i==j){break}
+        b=b+1
+      }
+    }
+  }
+  if(a==4){
+    cat("猜對了!!!")
+    break
+  }else{cat(a,"A",b,"B")}
+  
+  
+}
+
 
 
 
