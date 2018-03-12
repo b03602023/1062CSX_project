4f09d5c115c1ea5d6f6d76aa23d5b6d3865eff68
b03602023 <b03602023@ntu.edu.tw>
2018-03-12 21:48:36 +0800

    0312

--- week_2/course_2/self-practice for PChome crawler.R
+++ week_2/course_2/self-practice for PChome crawler.R
@@ -2,6 +2,18 @@
 library(httr)
 
 url <- "http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=sony&page=1&sort=rnk/dc"
+# 傳送get的請求(request)，取得網頁資訊response
+# get response
+res = GET(url)
+# 若成功獲得response則狀態碼會顯示200 
+# status: 200表示正常,404表示找不到網頁
+# 查看content-type
+
+res_json = content(res)
+do.call(rbind,res_json$prods)
+View(data.frame(do.call(rbind,res_json$prods)))
+
+url="https://ecshweb.pchome.com.tw/search/v3.3/all/categories?q=sony"
 res = GET(url)
 res_json = content(res)
 do.call(rbind,res_json$prods)
---
+++ week_2/course_2/self-practice for PTT movies crawler.R
@@ -0,0 +1,24 @@
+rm(list=ls(all.names=TRUE))
+library(XML)
+library(RCurl)
+library(httr)
+#改變R目前使用的語系（暫時有效；重新啟動R之後此設定就會消失）
+#中文換為英文： Sys.setlocale(category = "LC_ALL", locale = “UTF-8")
+#英文換中文
+Sys.setlocale(category = "LC_ALL", locale = "cht")
+urlPath <- "https://www.ptt.cc/bbs/movie/index.html"
+
+# Download a URI {RCurl}
+# 獲得網頁的原始碼
+temp    <- getURL(urlPath, encoding = "utf-8")
+##下載後的內容是HTML格式，無法直接分析。所以利用XML套件的readHTMLTable和XPath的功能來將需要的資訊從文件中萃取出來。
+xmldoc  <- htmlParse(temp)
+title   <- xpathSApply(xmldoc, "//div[@class=\"title\"]", xmlValue)
+title   <- gsub("\n", "", title)
+title   <- gsub("\t", "", title)
+author  <- xpathSApply(xmldoc, "//div[@class='author']", xmlValue)
+path    <- xpathSApply(xmldoc, "//div[@class='title']/a//@href")
+date    <- xpathSApply(xmldoc, "//div[@class='date']", xmlValue)
+response<- xpathSApply(xmldoc, "//div[@class='nrec']", xmlValue)
+alldata <- data.frame(title, author, path, date, response)
+write.table(alldata, file = "movie.csv")
